58461d98918c29e747e7d216a92390c9dcb27c0c
Rohit Jagga <rohitjagga02@gmail.com>
2021-08-13 15:02:19 -0400

    updating navbar

--- src/components/Contact/EmailLink.js
+++ src/components/Contact/EmailLink.js
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 import React, { useState, useEffect, useRef } from 'react';
 
 // Validates the first half of an email address.
@@ -9,21 +10,10 @@ const validateText = (text) => {
 };
 
 const messages = [
-  'hi',
-  'hello',
-  'hola',
-  'you-can-email-me-at-literally-anything! Really',
-  'well, not anything. But most things',
-  'like-this',
-  'or-this',
-  'but not this :(  ',
-  'you.can.also.email.me.with.specific.topics.like',
-  'just-saying-hi',
-  'please-work-for-us',
-  'help',
-  'admin',
-  'or-I-really-like-your-website',
-  'thanks',
+  'rohitjagga02@gmail.com',
+  'rjagga@wharton.upenn.edu',
+  'rohitjagga02@gmail.com',
+  'rjagga@wharton.upenn.edu',
 ];
 
 const useInterval = (callback, delay) => {
@@ -53,32 +43,36 @@ const EmailLink = () => {
   const [char, updateChar] = useState(messages[idx].length); // points to current char
   const [isActive, setIsActive] = useState(true); // disable when all messages are printed
 
-  useInterval(() => {
-    let newIdx = idx;
-    let newChar = char;
-    if (char - hold >= messages[idx].length) {
-      newIdx += 1;
-      newChar = 0;
-    }
-    if (newIdx === messages.length) {
-      setIsActive(false);
-    } else {
-      updateMessage(messages[newIdx].slice(0, newChar));
-      updateIter(newIdx);
-      updateChar(newChar + 1);
-    }
-  }, isActive ? delay : null);
+  useInterval(
+    () => {
+      let newIdx = idx;
+      let newChar = char;
+      if (char - hold >= messages[idx].length) {
+        newIdx += 1;
+        newChar = 0;
+      }
+      if (newIdx === messages.length) {
+        setIsActive(false);
+      } else {
+        updateMessage(messages[newIdx].slice(0, newChar));
+        updateIter(newIdx);
+        updateChar(newChar + 1);
+      }
+    },
+    isActive ? delay : null
+  );
 
   return (
     <div
       className="inline-container"
-      style={validateText(message) ? {} : { color: 'red' }}
+      // style={validateText(message) ? {} : { color: 'red' }}
+      style={validateText(message) ? {} : {}}
       onMouseEnter={() => setIsActive(false)}
-      onMouseLeave={() => (idx < messages.length) && setIsActive(true)}
+      onMouseLeave={() => idx < messages.length && setIsActive(true)}
     >
-      <a href={validateText(message) ? `mailto:${message}@mldangelo.com` : ''}>
+      <a href={validateText(message) ? `mailto:${message}` : ''}>
         <span>{message}</span>
-        <span>@mldangelo.com</span>
+        {/* <span>@mldangelo.com</span> */}
       </a>
     </div>
   );
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,17 @@
 /* eslint-disable operator-linebreak */
 // TODO Add a couple lines about each project
 const data = [
+  {
+    title: 'Tutoring Analytics Dashboard',
+    subtitle: 'NC Virtual Public Schools (NCVPS) Peer Tutoring Center',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2019-12-20',
+    desc:
+      'Built for a social impact hackathon. ' +
+      'NearestDollar connected to your bank accounts, credit cards, ' +
+      'or debit cards and rounded up your purchases to donate the balance to ' +
+      'the charity of your choice.',
+  },
   // {
   //   title: 'Nearest Dollar',
   //   subtitle: '2015 BVP Hackathon',
